43a0f7c5677bfc52e7633421ccb75eb5842f2b5c
Viktor Eriksson <viktor@komunike.se>
2015-03-19 08:37:53 +0100

    Initial commit

---
+++ dev_src/om_dropdown/dev.cljs
@@ -0,0 +1,10 @@
+(ns om-dropdown.dev
+    (:require
+     [om-dropdown.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
