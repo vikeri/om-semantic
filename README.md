121da805850627560ecb6d1e2136c494dbf4dd81
Viktor Eriksson <viktor@komunike.se>
2015-03-20 14:03:08 +0100

    Removed figwheel for production

---
+++ README.md
@@ -0,0 +1,24 @@
+## Om-Semantic
+
+[Semantic UI](https://github.com/semantic-org/semantic-ui/) components built with [Om](https://github.com/omcljs/om)
+
+Still very much in Alpha, use at own risk.
+
+## Available components
+
+- Dropdown `dropdown.cljs`
+
+
+## Try it out
+
+```sh
+git clone https://github.com/vikeri/om-semantic.git
+cd om-semantic
+lein cljsbuild once dropdown
+```
+
+Open `index.html` in folder `examples/dropdown`
+
+## License
+
+Licensed under the EPL
--- project.clj
+++ project.clj
@@ -7,12 +7,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-3126"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [figwheel "0.2.5-SNAPSHOT"]
                  [prismatic/om-tools "0.3.11"]
                  [org.omcljs/om "0.8.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.5-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.5"]]
 
   :source-paths ["src"]
 
@@ -20,34 +18,9 @@
   
   :cljsbuild {
     :builds [{:id "dropdown"
-              :source-paths ["src" "examples/dropdown" "dev_src"]
-              :compiler {:output-to "resources/public/js/compiled/om_semantic.js"
-                         :output-dir "resources/public/js/compiled/out/dropdown"
+              :source-paths ["src" "examples/dropdown/src"]
+              :compiler {:output-to "examples/dropdown/main.js"
+                         :output-dir "examples/dropdown/out"
                          :optimizations :none
-                         :main om-semantic.dev
-                         :asset-path "js/compiled/out/dropdown"
-                         :source-map true
-                         :source-map-timestamp true
-                         :cache-analysis true }}
-             {:id "dev"
-              :source-paths ["src" "dev_src"]
-              :compiler {:output-to "resources/public/js/compiled/om_semantic.js"
-                         :output-dir "resources/public/js/compiled/out"
-                         :optimizations :none
-                         :main om-semantic.dev
-                         :asset-path "js/compiled/out"
-                         :source-map true
-                         :source-map-timestamp true
-                         :cache-analysis true }}
-             {:id "min"
-              :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/om_semantic.js"
-                         :main om-semantic.core                         
-                         :optimizations :advanced
-                         :pretty-print false}}]}
-
-  :figwheel {
-             :http-server-root "public" ;; default and assumes "resources"
-             :server-port 3449 ;; default
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-             })
+                         :source-map true}}]}
+)
