5938e949762872a7da857ffc6211322175b48ded
Sebastian Bensusan <sbensu@gmail.com>
2015-04-04 19:10:59 +0200

    Added first test and refactored a little

--- test/tests.cljs
+++ test/tests.cljs
@@ -1,12 +1,39 @@
 (ns om-semantic.tests
   (:require [cljs.test :refer-macros [is testing deftest]]
+            [om-semantic.dropdown :as d]
+            [om.core :as om :include-macros true]
+            [dommy.core :as dommy :refer-macros [sel]]
             [cljs-react-test.utils :as tu]))
 
 (enable-console-print!)
 
-(deftest basic-test-handling 
-  (testing "Simple test"
-    (tu/new-container!)
-    (is (= 1 1))))
+(deftest dropdown 
+  (testing "Dropdown displays initial state"
+    (let [c (tu/new-container!)
+          xs [{:id 1 :val "Joffrey"}
+              {:id 2 :val "Cersei"}
+              {:id 5 :val "Tywin"}]
+          app-state (atom {:xs xs :selected 1})
+          init-state {:menu [:xs]
+                      :idkey :id
+                      :lkey :val
+                      :disabled false
+                      :selected [:selected]}
+          wrapper (fn [data owner]
+                    (om/component
+                     (om/build d/dropdown data {:init-state init-state})))
+          root (om/root wrapper app-state {:target c})
+          [main-node display-node] (sel c [:div])]
+      (is (= "Joffrey" (.-innerHTML display-node)))
+      (testing "and opens on click, showing the options"
+        (tu/click main-node)
+        (om/render-all)
+        (let [ns (sel c [:div :.menu :div])]
+          (is (= (map :val xs) (map #(.-innerHTML %) ns)))
+          (testing "which when clicked change the state"
+            (tu/click (second ns))
+            (om/render-all)
+            (is (= (:selected @app-state) (:id (second xs))))
+            (is (= (:val (second xs)) (.-innerHTML display-node)))))))))
 
 
